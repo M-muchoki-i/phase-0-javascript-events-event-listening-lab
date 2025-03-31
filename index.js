37041d984523b8165c1e514104d5a800f934941e
michelle <michellemuchoki8@gmail.com>
2025-03-31 19:55:17 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
+   function addingEventListener() {
+     const input = document.getElementById("button");
+
+     function clickAlert() {
+       alert("I was clicked!");
+     }
+
+     input.addEventListener("click", clickAlert);
+   }
+
+   
+   addingEventListener();
+
+
 }
+ 
